2f1effa471d045e42c17c6a99182552068565539
Christian Madajski <cmadajsk@uncc.edu>
2021-11-30 16:59:53 -0500

    FinanceTracker v1 - added initial app files

---
+++ Display.py
@@ -0,0 +1,5 @@
+# contains all the CLI display formatting
+def mainMenu():
+    print('FINANCE TRACKER CLI')
+    print('1. new transaction')
+    print('2. ')
---
+++ main.py
@@ -0,0 +1,11 @@
+from Transaction import *
+from Display import *
+
+
+# program execution starts here
+def main():
+    mainMenu()
+
+
+if __name__ == '__main__':
+    main()
